49a7f9feb07e2c9c359768bbb570541d869f8142
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-03-31 20:48:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	let str = "";
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] != " ") {
+			str.push(s[i]);
+		} else if (s[i] == " ") {
+			return str;
+		} 
+	}
+	return str;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
